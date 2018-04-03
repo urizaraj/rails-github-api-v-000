546644dd3e3bda092c3df7fb8a148c681c5b10e3
Alexander Urizar <urizaraj@gmail.com>
2018-04-03 13:18:46 -0400

    changes

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,28 @@
 class RepositoriesController < ApplicationController
+  require 'json'
   def index
-    @username = session[:username]
+    resp = Faraday.get do |req|
+      req.url 'https://api.github.com/user/repos'
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @body = JSON.parse(resp.body)
   end
 
   def create
+    redirect_back if params[:name].empty?
+
+    resp = Faraday.post do |req|
+      req.url 'https://api.github.com/user/repos'
+      req.body = {name: params[:name]}.to_json
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(resp.body)
+
+    flash[:message] = "Created #{ body['name'] } at #{ body['html_url'] }"
+    redirect_to root_path
   end
 end
